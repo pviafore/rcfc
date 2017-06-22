edd4f7a3ddf1fcdbc4c43b3f9a5128c7d3ba8828
Pat Viafore <patviafore@gmail.com>
2017-06-21 22:01:06 -0500

    Starting packaging framework

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest',
+      version='0.1',
+      description='A framework that gives a remote control for computers that is Python driven',
+      url='http://github.com/pviafore/rcfc',
+      author='Pat Viafore',
+      author_email='patviafore+rcfc@gmail.com',
+      license='MIT',
+      packages=['rcfc'],
+      zip_safe=False)
