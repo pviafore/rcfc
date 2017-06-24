bbf14946c092686c54a29cc7e240f765707974b7
Pat Viafore <patviafore@gmail.com>
2017-06-24 14:35:57 -0500

    Messing with setup.py again

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ with open("requirements.txt", "r") as reqs:
     requirements = reqs.readlines()
 
 setup(name='rcfc',
-      version='0.2.3',
+      version='0.2.4',
       description='A framework that gives a remote control for computers that is Python driven',
       url='http://github.com/pviafore/rcfc',
       author='Pat Viafore',
       author_email='patviafore+rcfc@gmail.com',
       license='MIT',
       packages=['rcfc'],
+      package_data={'rcfc': ['static/*']},
       install_requires=requirements,
       zip_safe=False)
